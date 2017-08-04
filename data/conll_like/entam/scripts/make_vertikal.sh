1a4bdcdd7b9e6505f593e08785808ab5bf8f7ff0
Anša Vernerová <ansa.ansa@gmx.net>
2017-08-04 16:18:19 +0200

    EnTam corpus
    English-Tamil corpus
    
    remove its duplicate in the "identic" directory
    
    note that the corpus is currently not uploaded either to kontext-dev nor to kontext

--- data/conll_like/entam/scripts/make_vertikal.sh
+++ data/conll_like/entam/scripts/make_vertikal.sh
@@ -10,7 +10,7 @@ do
 	outfile='../output/EnTam_'$lang'-w'
 	echo "Echo into $outfile"
 	echo "<doc>" > $outfile
-	cat -n $input_dir/corpus.bcn.train.$lang | sed 's/^ *\([0-9][0-9]*\)\t\(.*\)/<align_id="\1">\n\2\n<\/align>/' | sed 's/ /\n/g'  | sed -e '/<s>/i <\/s>' -e '/<\/align>/i <\/s>' -e '/<align/a <s>' | sed 's/align_id/align id/g'  >> $outfile 
+	cat -n $input_dir/corpus.bcn.train.$lang | sed 's/^ *\([0-9][0-9]*\)\t\(.*\)/<align id="\1">\n\2\n<\/align>/' | sed '/|/s/ /\n/g;s/|/\t/g' |sed -e '/<s>/i <\/s>' -e '/<\/align>/i <\/s>' -e '/<align/a <s>' >> $outfile 
 	echo "</doc>" >> $outfile
 #sed 's/^ *\([0-9][0-9]*\)\t\(.*\)$$$$/<align id="\1">\n\2\n<\/align>/' | sed '/|/s/ /\n/g;s/|/\t/g' | cut -f1,2,4 | sed -e '/<s>/i <\/s>' -e '/<\/align>/i <\/s>' -e '/<align/a <s>'
 done
