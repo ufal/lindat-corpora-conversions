4ec80629e71e752cff8161188b2fcd17d542192f
Anša Vernerová <vernerova@ufal.mff.cuni.cz>
2018-04-12 20:08:45 +0200

    UD 2.2: now it works, except for fused tokens

--- data/treex/universal_dep/scripts/ud_convert.sh
+++ data/treex/universal_dep/scripts/ud_convert.sh
@@ -1,20 +1,22 @@
 #/bin/bash
 #ROOT_FOLDER=/net/cluster/TMP/vernerova
-ROOT_FOLDER=..
+ROOT_FOLDER=~/Lindat-corpora-conversions/data/treex/universal_dep
 TRAIN_FOLDER=$ROOT_FOLDER/input
 OUT_FOLDER=$ROOT_FOLDER/output
 
 
 function generate_vertical {
-    echo "==========generating VERITCAL from file: $1========";
+    echo "==========generating VERITCAL from dir: $1========";
     echo "language: $2";
 	cd $OUT_FOLDER;
     #treex -L$2 --parallel --mem=8G Read::CoNLLU from=$1 Write::ManateeGeneral format=UD2.2 to=.
-    treex -L$2 Read::CoNLLU from=$1 Write::ManateeGeneral format=UD2.2 to=.
+    files=$(echo $1/*{train,dev}.conllu)
+    echo "processing $files, lang: $lang_truncated, saving to $(basename $1)";
+    treex -L$2 Read::CoNLLU from="$files" format=UD2.2 Write::ManateeGeneral format=UD2.2 to=$(basename $1) unique_ids=1
 }
 
 
-for dir in $TRAIN_FOLDER/UD_*; do
+for dir in $TRAIN_FOLDER/TEST; do    ## TODO: change to $TRAIN_FOLDER/UD_*
   cd $ROOT_FOLDER/scripts
   dirname=${dir#*UD_}
   language=${dirname%%-*}
@@ -27,14 +29,10 @@ for dir in $TRAIN_FOLDER/UD_*; do
     filename=${filename%%-*}
     languagecode=${filename%%_*}
     echo "$languagecode $language"
-    python generate_templates.py "$filename" "$languagecode" "$language" "$corpname"
-    for file in $dir/*{train,dev}.conllu; do
-      filename=$(basename "$file")
-      lang=$(echo $filename | cut -f1 -d-)
-      lang_truncated=$(echo "$lang" | sed 's/_.*//g');
-      echo "processing $file, lang: $lang_truncated";
-      generate_vertical $file $lang_truncated $lang; 
-    done
+    #python generate_templates.py "$filename" "$languagecode" "$language" "$corpname"
+    lang=$(echo $filename | cut -f1 -d-)
+    lang_truncated=$(echo "$lang" | sed 's/_.*//g');
+    generate_vertical $dir $lang_truncated $lang; 
   fi
 done
 
