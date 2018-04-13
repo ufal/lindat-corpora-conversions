9477a15afa8687dc9a144a5073b86c4a83783c88
Anša Vernerová <vernerova@ufal.mff.cuni.cz>
2018-04-13 09:49:43 +0200

    UD 2.2: run on the real data

--- data/treex/universal_dep/scripts/ud_convert.sh
+++ data/treex/universal_dep/scripts/ud_convert.sh
@@ -6,17 +6,17 @@ OUT_FOLDER=$ROOT_FOLDER/output
 
 
 function generate_vertical {
-    echo "==========generating VERITCAL from dir: $1========";
-    echo "language: $2";
+    echo "==========generating VERITCAL from dir: $1========" > log;
+    echo "language: $2" > log;
 	cd $OUT_FOLDER;
-    #treex -L$2 --parallel --mem=8G Read::CoNLLU from=$1 Write::ManateeGeneral format=UD2.2 to=.
     files=$(echo $1/*{train,dev}.conllu)
-    echo "processing $files, lang: $lang_truncated, saving to $(basename $1)";
-    treex -L$2 Read::CoNLLU from="$files" format=UD2.2 Write::ManateeGeneral format=UD2.2 to=$(basename $1) unique_ids=1
+    echo "processing $files, lang: $lang_truncated, saving to $(basename $1)" > log;
+    treex -L$2 --parallel --mem=8G Read::CoNLLU from="$files" Write::ManateeGeneral format=UD2.2  to=$(basename $1) unique_ids=1
+    #treex -L$2 Read::CoNLLU from="$files" Write::ManateeGeneral format=UD2.2 to=$(basename $1) unique_ids=1
 }
 
 
-for dir in $TRAIN_FOLDER/TEST; do    ## TODO: change to $TRAIN_FOLDER/UD_*
+for dir in $TRAIN_FOLDER/UD_*; do
   cd $ROOT_FOLDER/scripts
   dirname=${dir#*UD_}
   language=${dirname%%-*}
@@ -28,7 +28,7 @@ for dir in $TRAIN_FOLDER/TEST; do    ## TODO: change to $TRAIN_FOLDER/UD_*
     filename=$(basename $dir/*train.conllu .conllu)
     filename=${filename%%-*}
     languagecode=${filename%%_*}
-    echo "$languagecode $language"
+    echo "$languagecode $language > log"
     #python generate_templates.py "$filename" "$languagecode" "$language" "$corpname"
     lang=$(echo $filename | cut -f1 -d-)
     lang_truncated=$(echo "$lang" | sed 's/_.*//g');
