e1d72df20258d6d2863143f42b57b0e41e491e9b
Anša Vernerová <vernerova@ufal.mff.cuni.cz>
2018-06-24 20:04:01 +0200

    Summa: prepare plaintext for parsing (unfinished)
    note that a similar solution was simultaneously developed by shark
    and may be found in the custom/oxford submodule

---
+++ data/conll_like/summa/scripts/create_plaintext.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+#use diagnostics;
+use locale;    # sorts natural text, not binary data:
+# _0123456789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ
+#no locale;     # sorts as binary data:
+# 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz
+use POSIX qw(locale_h);    # Import locale-handling tool set from POSIX module; necessary for use of setlocale
+setlocale(LC_ALL, qw(cs_CZ.UTF8));   # for the time being, set locale to Czech UTF-8; affects what is considered alphanumeric character in regexp, e.g. "\w"
+## you can change the locale at runtime, e.g.:
+#$old_locale = setlocale(LC_CTYPE); setlocale(LC_CTYPE, qw(cs_CZ.UTF8));
+## restore the old locale with
+#setlocale(LC_CTYPE, $old_locale);
+setlocale(LC_NUMERIC, qw(en_EN.UTF8));   # influnces decimal comma/point
+setlocale(LC_COLLATE, qw(cs_CZ.UTF8));   # ordering
+
+use utf8;                      # now you may use UTF-8 in the source code!
+binmode STDOUT, ':encoding(utf8)';
+binmode STDIN, ':encoding(utf8)';
+use feature 'unicode_strings';  # full internal Unicode support: interpret internal representation of strings as utf8 (but does not change the internal representation!)
+
+use Encode qw(encode decode);     # for handling input and output (including command-line arguments): en/decode('utf8',$string)
+
+### Encode is also useful with the CGI module and UTF-8 input, because in the following case, $x is not considered tainted:
+## #!/usr/bin/perl -T
+## use CGI qw(:standard -utf8);   # the -utf8 switch causes $x to be untainted
+## my $x=param('x');
+## open(OUT, ">$x");
+## print OUT "$x";
+
+### while in the following case, $x is considered tainted and running the script throws an error
+## #!/usr/bin/perl -T
+## use CGI qw(:standard);
+## use Encode qw(decode);
+## my $x=param('x');
+## $param = decode('UTF-8', $param);
+## open(OUT, ">$x");
+## print OUT "$x";
+
+
+use feature qw(switch say);
+
+# Debuging...
+my $debug = 1;
+sub _debug {
+  if ($debug) { say join "\n", @_; }
+}
+#sub _debug {
+#  my $debug = shift; # == 1   -> output (add_line)
+#                     # == 2   -> main (reading of the vallex.txt file)
+#  if ($debug>1) { 
+#    if ( defined((caller(1))[3]) ) { 
+#      print STDERR "\n\n", (caller(1))[3], ":\n", @_; 
+#    } else {
+#      print STDERR "\n\n", "main:\n", @_; 
+#    }
+#  }
+#}
+
+foreach my $filename (@ARGV) {
+  open SOURCE, "<:encoding(utf-8)", $filename;
+  open PLAIN, ">:encoding(utf-8)", "$filename.plaintext";
+  open XML, ">:encoding(utf-8)", "$filename.xml_positions";
+  my $position = 0;
+  while (<SOURCE>) {
+    chomp;
+    my ($id, $text) = split /\t/;
+    say XML "<seg id=\"$id\">\t$position" unless $text =~ m/<pars id="[^"]*">|<\/pars>;/;
+    my @text = split /(<[^>]*>)/, $text;
+      my $i=0;
+      while ($i<=$#text) {
+        print PLAIN "$text[$i]" if $text[$i];
+        $position+=length($text[$i]);
+        if ($i+1<=$#text) {
+          say XML "$text[$i+1]\t".$position;
+          if ($text[$i+1] =~ m!</[hp]>!) {
+            say PLAIN "\n"; $position+=2;
+          }
+        }
+        $i+=2;
+      }      
+    say XML "</seg>\t$position" unless $text =~ m/<pars id="[^"]*">|<\/pars>;/;
+    say PLAIN ""; $position++;
+  }
+}
+
+
+1;
+## we need the TRUE value as the last thing to tell Perl that everything went
+##   OK when loading the file
+## this would be relevant especially if the module contains any code that is not
+##   part of any subroutine as such code is executed at the time the module is loaded
+## the default return value of loading a module is 0!
+## thus a module which only contains subroutines would fail when "require"d or "use"d in another module
